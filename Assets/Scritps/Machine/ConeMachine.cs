eb36cc6f3b92063d4736c9a0892be3b2d46ecb4b
Sahapat <sahapat.starcaft@gmail.com>
2017-12-11 23:52:14 +0700

    fix bug(Now can't placeObject if it's not empty)
    Create canSuccess and use it to check on finish.

--- Assets/Scritps/Machine/ConeMachine.cs
+++ Assets/Scritps/Machine/ConeMachine.cs
@@ -6,21 +6,31 @@ public class ConeMachine : Machine
 {
     [SerializeField]
     private Vector3 spawnPoint;
-
     public int ConeType;
     public GameObject[] ConeObj;
+    public bool canSuccess;
 
     private void Start()
     {
-        workingTime = 3;
+        workingTime = 1.4f;
         maxSlot = 1;
+        canSuccess = true;
     }
-    protected override void OnFinishWorking()
+    protected override bool OnFinishWorking()
     {
-        GameObject temp = Instantiate(ConeObj[ConeType], spawnPoint, Quaternion.identity);
-        Cone cone = temp.GetComponent<Cone>();
-        cone.setFlavor(ConeType);
-        cone.LastPosition = spawnPoint;
+        if(canSuccess)
+        {
+            GameObject spawnObject = Instantiate(ConeObj[ConeType], spawnPoint, Quaternion.identity);
+            Cone cone = spawnObject.GetComponent<Cone>();
+            cone.setFlavor(ConeType);
+            cone.LastPosition = spawnPoint;
+            canSuccess = false;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
     public override void OnAddProduct()
     {
