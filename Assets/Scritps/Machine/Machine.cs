23fdaa4fb16f55f4ee801c2e8cc4062cf127b3d7
Sahapat <sahapat.starcaft@gmail.com>
2017-12-12 14:19:59 +0700

    Fix bug (Now can't placeObject if it's not empty)on base class
    change OnFinishWorking with bool return >> return true if empty and false is not empty

--- Assets/Scritps/Machine/Machine.cs
+++ Assets/Scritps/Machine/Machine.cs
@@ -5,13 +5,14 @@ using UnityEngine;
 public abstract class Machine : MonoBehaviour
 {
     [SerializeField]
-    protected int workingTime = 0;
+    protected float workingTime = 0;
     [SerializeField]
     protected int maxSlot = 0;
     [SerializeField]
     private AudioClip AddToMachine;
     [SerializeField]
     private AudioClip SuccessProduct;
+
     public int avilableSlot = 0;
     private bool _isWorking = false;
     public bool isWorking
@@ -26,11 +27,13 @@ public abstract class Machine : MonoBehaviour
             anim.SetBool("isWorking", value);
         }
     }
+    protected bool isSuccess = false;
+    [SerializeField]
     private int timeCount = 0;
     protected Animator anim;
     protected AudioSource audioSource;
 
-    public void Add()
+    public bool Add()
     {
         if (avilableSlot < maxSlot)
         {
@@ -38,7 +41,9 @@ public abstract class Machine : MonoBehaviour
             OnAddProduct();
             isWorking = true;
             avilableSlot++;
+            return true;
         }
+        else return false;
     }
     private void Awake()
     {
@@ -49,21 +54,19 @@ public abstract class Machine : MonoBehaviour
     {
         if(isWorking)
         {
-            if(timeCount == 0)
+            timeCount = (timeCount > workingTime*100)?timeCount:timeCount+1;
+            if(timeCount >= workingTime * 100)
             {
-                OnStartWorking();
-            }
-            timeCount += 1;
-            if(timeCount == workingTime*100)
-            {
-                avilableSlot -= 1;
-                if(avilableSlot == 0)
+                if(OnFinishWorking())
                 {
-                    isWorking = !isWorking;
+                    avilableSlot -= 1;
+                    if(avilableSlot == 0)
+                    {
+                        isWorking = !isWorking;
+                    }
+                    timeCount = 0;
+                    audioSource.PlayOneShot(SuccessProduct);
                 }
-                timeCount = 0;
-                OnFinishWorking();
-                audioSource.PlayOneShot(SuccessProduct);
             }
         }
     }
@@ -71,9 +74,5 @@ public abstract class Machine : MonoBehaviour
     {
         print("Add Sucess");
     }
-    protected virtual void OnStartWorking()
-    {
-        print("Start Working");
-    }
-    protected abstract void OnFinishWorking();
+    protected abstract bool OnFinishWorking();
 }
